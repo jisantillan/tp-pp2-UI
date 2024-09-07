2152c6cc5e4a2590d6a8d2c401e31a9c6c2b4102
Kevops <kruiz@sysone.com>
2024-09-07 13:41:41 -0300

    feat(): improve pom with flatten maven plugin

--- src/main/java/org/domingus/Main.java
+++ src/main/java/org/domingus/Main.java
@@ -1,7 +1,10 @@
 package org.domingus;
 
+import org.domingus.core.utils.CommandType;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println(CommandType.CONTACT.name());
         System.out.println("Hello world!");
     }
 }
