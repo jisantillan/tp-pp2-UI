6563073f91f1b57a68432c7a43ca6c3918cd741d
lc-sanchez <csanchez_98@outlook.com>
2024-09-15 23:38:57 -0300

    feat(init view)-added ui classes

--- src/main/java/org/domingus/DomingusUI.java
+++ src/main/java/org/domingus/DomingusUI.java
@@ -1,11 +1,29 @@
 package org.domingus;
 
 
-import org.domingus.app.InformationType;
+import org.domingus.ui.View;
+
+import java.beans.PropertyChangeEvent;
 
 public class DomingusUI {
     public static void main(String[] args) {
-        System.out.println(InformationType.ACADEMIC_OFFER.name());
-        System.out.println("Hello world!");
+        //Aca se deberia inicializar usando el json
+
+        //Domingus domingus = new DomingusFactory(archivo json).create();
+        View view = new View();
+        view.init();
+
+        for (int i = 1; i <= 20; i++) {
+            PropertyChangeEvent event = new PropertyChangeEvent(view, "notificacion", null, "Mensaje " + i);
+            view.propertyChange(event);
+
+            // Para simular
+            try {
+                Thread.sleep(1000);  // 1 segundo entre notificaciones
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
