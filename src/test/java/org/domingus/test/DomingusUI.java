e9d7f69710e5f8dd75b746527aa1951952171f43
kruiz <kruiz@sysone.com>
2024-11-07 20:13:30 -0300

    feat(): receive timeInterval from args

--- src/test/java/org/domingus/test/DomingusUI.java
+++ src/test/java/org/domingus/test/DomingusUI.java
@@ -9,29 +9,40 @@ import org.domingus.interfaces.Source;
 import org.domingus.logger.Logger;
 import org.domingus.ui.DomingusView;
 
+import static java.util.Objects.nonNull;
+
 public class DomingusUI {
 
 	private static String EXTENSIONS_PATH = "src\\test\\resources\\extensions\\";
 	private static String MEMORY_PATH = "src\\test\\resources\\memory.txt";
-	private static Integer TIME_INTERVAL = 2000;
-	
+
     public static void main(String[] args) throws IOException {
     	
 		Source source = new SourceUI();
-		TimerUI timer = new TimerUI(TIME_INTERVAL, (Runnable) source);
-		
-		DomingusFactory factory = new DomingusFactory();
-		Domingus domingus = factory.create(source, EXTENSIONS_PATH);
-		
-		LoggerFactory loggerFactory = new LoggerFactory();
-		Logger logger = loggerFactory.create(MEMORY_PATH);
 
-		domingus.addObserver(logger);
+		Integer timeInterval = null;
+		try	{
+			timeInterval = Integer.valueOf(args[0]);
+		} catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+			System.out.println("Error obteniendo los segundos por parámetro");
+		}
 
-		DomingusView domingusView = new DomingusView(domingus);
-		domingusView.init();
+		if(nonNull(timeInterval)) {
+			TimerUI timer = new TimerUI(timeInterval, (Runnable) source);
 
-		timer.run();
+			DomingusFactory factory = new DomingusFactory();
+			Domingus domingus = factory.create(source, EXTENSIONS_PATH);
+
+			LoggerFactory loggerFactory = new LoggerFactory();
+			Logger logger = loggerFactory.create(MEMORY_PATH);
+
+			domingus.addObserver(logger);
+
+			DomingusView domingusView = new DomingusView(domingus);
+			domingusView.init();
+
+			timer.run();
+		}
     }
 
 }
